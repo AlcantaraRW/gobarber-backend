d673363b3dd9a2aa99848e02654c58d9dac414fe
Rafael Alcântara <rafael_william_@hotmail.com>
2020-05-13 19:42:15 -0300

    Implemented Amazon SES mail provider.

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'emailconfiguradonaaws@example.com',
+      name: 'Rafael Alcântara',
+    },
+  },
+} as IMailConfig;
